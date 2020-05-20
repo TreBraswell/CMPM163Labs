489715946dcb1ea896f987004a9183aec6792f4b
TreBraswell <49354031+TreBraswell@users.noreply.github.com>
2020-05-20 00:47:18 -0700

    final

---
+++ lab7/README.txt
@@ -0,0 +1,6 @@
+I chose part 1
+I made the water by using the sin function on the world positions
+I created the mountains by following the diagram
+I also added the extra by using the world position to change shapes I created three shapes a cube, sphere, and quad they move up and down left and right and then right to left
+https://drive.google.com/file/d/1iFTRpay88uWXc5xtJBhlq80CYHKIzus-/view?usp=sharing
+https://github.com/TreBraswell/CMPM163Labs
---
+++ lab7/lab 7/Assets/enemy_movement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_movement : MonoBehaviour
+{
+    public float thrust = 1.1f;
+    public float waitTime = 2.0f;
+    private Rigidbody self;
+    public float pausetime = 3.0f;
+    //private float timer = 0.0f;
+    public float endpos = 4.0f;
+    public Vector3 start; 
+    public bool moveleftright = false;
+    public bool moverightleft = false;
+    public bool moveupdown = false;
+    //private bool swit = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        start = this.gameObject.transform.position;
+        self = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (moverightleft)
+        {
+            if (start.x + endpos < this.gameObject.transform.position.x)
+            {
+                self.AddForce(-transform.right * thrust);
+            }
+            else if (start.x + endpos> this.gameObject.transform.position.x)
+            {
+                self.AddForce(transform.right * thrust);
+            }
+        } 
+        else if (moveleftright)
+        {
+            if (start.x - endpos > this.gameObject.transform.position.x)
+            {
+                self.AddForce(transform.right * thrust);
+            }
+            else if (start.x - endpos < this.gameObject.transform.position.x)
+            {
+                self.AddForce(-transform.right * thrust);
+            }
+        }
+        else if (moveupdown)
+        {
+            if (start.y + endpos < this.gameObject.transform.position.y)
+            {
+                self.AddForce(-transform.up * thrust);
+            }
+            else if (start.y + endpos > this.gameObject.transform.position.y)
+            {
+                self.AddForce(transform.up * thrust);
+            }
+        }
+        
+    }
+}
